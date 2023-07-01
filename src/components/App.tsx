41d5cd3cb30618e7d0369f8c2cd5535eba378ab3
Grace de la Mora <thetrend@users.noreply.github.com>
2023-07-01 00:39:49 -0400

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+};
+
+export default App;
